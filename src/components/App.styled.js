e1aca35e3f63bfb535e2acc284fe3bd568cdb7c3
SkyMolfar <skymolfars@gmail.com>
2023-07-14 18:11:52 +0200

    push\

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -2,18 +2,24 @@ import styled from 'styled-components';
 
 
 export const Container = styled.div`
-    height: 500px;
-    width: 1000px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 40px;
-    color: #010101;
-    border-radius: 3px;
-    border: 1px solid blue;
-    background-color: gainsboro;
-    margin: 0 auto;
-    gap: 50px;
+    position: fixed;
+top: 50%;
+left: 0;
+right: 0;
+transform: translateY(-50%);
+height: 500px;
+width: 1000px;
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: 40px;
+color: #010101;
+border-radius: 3px;
+border: 1px solid blue;
+background-color: gainsboro;
+margin: auto;
+gap: 50px;
+
 `;
 
 export const NoFeedback = styled.p`
